628a35daabe88b5e17d35d1723bf41d31ef74d62
shivanya-namra <shivanyanamra30@gmail.com>
2026-06-19 21:10:57 +0530

    Add simple calculator program in C

---
+++ simple.c
@@ -0,0 +1,14 @@
+// simple calculator 
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    scanf("%d%d", &a, &b);
+
+    printf("Addition = %d\n", a + b);
+    printf("Subtraction = %d\n", a - b);
+    printf("Multiplication = %d\n", a * b);
+
+    return 0;
+}
