67a116c78bfdba44bf57d1155c238b586e1c97bb
shivanya-namra <shivanyanamra30@gmail.com>
2026-06-19 21:13:39 +0530

    Update comment for voting eligibility check

---
+++ Voting.c
@@ -0,0 +1,15 @@
+// Check Voting Eligibility
+
+#include <stdio.h>
+
+int main() {
+    int age;
+    scanf("%d", &age);
+
+    if(age >= 18)
+        printf("Eligible");
+    else
+        printf("Not Eligible");
+
+    return 0;
+}
