93a2165c091c3ff009a5bb764ffcb936216bc5a6
shivanya-namra <shivanyanamra30@gmail.com>
2026-06-19 20:20:07 +0530

    Add countdown program in countdown.c

---
+++ countdown.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+
+        int i,j;
+       
+        for(i=1; i <=5; i++)
+    {
+         for(j=1; j <=3; j++)
+    {
+        
+            printf("%d %d",i,j);
+            
+        }
