da2338db8a528bd0fb9ac154755ac7e6821cabac
shivanya-namra <shivanyanamra30@gmail.com>
2026-06-23 11:48:50 +0530

    Create integer.c

---
+++ integer.c
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+        cout << num << " is Even.";
+    else
+        cout << num << " is Odd.";
+
+    return 0;
+}
