d7ed897e7d2b85a2e7b8bc0b804f5080912e1b53
shivanya-namra <shivanyanamra30@gmail.com>
2026-06-29 12:16:54 +0530

    Create multipication.cpp

---
+++ multipication.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float num1, num2, product;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    product = num1 * num2;
+
+    cout << "Product = " << product;
+
+    return 0;
+}
